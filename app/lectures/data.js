41f670356bd5eb6d670ccaba0819716c7184e10a
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-06-12 21:59:06 -0500

    add attribute slug, next

--- app/lectures/data.js
+++ app/lectures/data.js
@@ -3,101 +3,136 @@ const activities = [
         "name": "Alfabeto",
         "course": 0,
         "unlock_score": 0,
-        "slug":"alfabeto"
+        "slug": "alphabet",
+        "next": "numbers",
     },
     {
         "course": "Básico",
         "course": 0,
         "name": "Números",
         "unlock_score": 1,
-        "slug":"numeros"
+        "slug": "numbers",
+        "next": "colors",
     },
     {
         "unlock_score": 2,
         "course": 0,
         "name": "Colores",
-        "slug":"colores"
+        "slug": "colors",
+        "next": "grammar-1",
     },
     {
         "unlock_score": 3,
         "course": 0,
         "name": "Gramática: Pronombres y Estructura Básica de Oraciones",
-        "slug":"gramatica-1"
+        "slug": "grammar-1",
+        "next": "game-a0",
     },
     {
         "unlock_score": 4,
         "course": 0,
-        "name": "Juego A0"
+        "name": "Juego A0",
+        "slug": "game-a0",
+        "next": "test-1",
     },
     {
         "unlock_score": 9,
         "course": 0,
-        "name": "Evaluación 1"
+        "name": "Evaluación 1",
+        "slug": "test-1",
+        "next": "greetings-farewells",
     },
     {
         "unlock_score": 14,
         "course": 0,
-        "name": "Saludos Cortesía y Despedidas"
+        "name": "Saludos Cortesía y Despedidas",
+        "slug": "greetings-farewells",
+        "next": "family",
     },
     {
         "unlock_score": 15,
         "course": 0,
-        "name": "La familia"
+        "name": "La familia",
+        "slug": "family",
+        "next": "animals",
     },
     {
         "unlock_score": 16,
         "course": 0,
-        "name": "Animales"
+        "name": "Animales",
+        "slug": "animals",
+        "next": "body",
     },
     {
         "unlock_score": 17,
         "course": 0,
-        "name": "Partes del Cuerpo"
+        "name": "Partes del Cuerpo",
+        "slug": "body",
+        "next": "grammar-2",
+
     },
     {
         "unlock_score": 18,
         "course": 0,
-        "name": "Cosas del Hogar"
+        "name": "Gramática: género, plural, tamaño, cantidad",
+        "slug": "grammar-2",
+        "next": "game-a1",
     },
     {
         "unlock_score": 19,
         "course": 0,
-        "name": "Juego A1"
+        "name": "Juego A1",
+        "slug": "game-a1",
+        "next": "test-2",
     },
     {
         "unlock_score": 20,
         "course": 0,
-        "name": "Evaluación 2"
+        "name": "Evaluación 2",
+        "slug": "test-2",
+        "next": "home",
+    },
+    {
+        "unlock_score": 34,
+        "course": 0,
+        "name": "Cosas del Hogar",
+        "slug": "home",
+        "next": "classroom",
     },
     {
         "unlock_score": 35,
         "course": 0,
-        "name": "El Aula"
+        "name": "El Aula",
+        "slug": "classroom",
+        "next": "food",
     },
     {
         "unlock_score": 36,
         "course": 0,
-        "name": "Alimentos"
-    },
-    {
-        "unlock_score": 37,
-        "course": 0,
-        "name": "Gramática: género, plural, tamaño, cantidad"
+        "name": "Alimentos",
+        "slug": "food",
+        "next": "grammar-3",
     },
     {
         "unlock_score": 38,
         "course": 0,
-        "name": "Gramática: Imperativo, Tiempo Presente"
+        "name": "Gramática: Imperativo, Tiempo Presente",
+        "slug": "grammar-3",
+        "next": "game-a2",
     },
     {
         "unlock_score": 39,
         "course": 0,
-        "name": "Juego C3"
+        "name": "Juego C3",
+        "slug": "game-a2",
+        "next": "test-3",
     },
     {
         "unlock_score": 40,
         "course": 0,
-        "name": "Evaluación 3"
+        "name": "Evaluación 3",
+        "slug": "game-a2",
+        "next": "/",
     },
 
 ]
