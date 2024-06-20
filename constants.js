c15212bed4ca5f0bf16182ba6fb21952c9b25db7
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-06-19 21:58:33 -0500

    update endpoint constants

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
-export const API_ROOT = 'http://34.67.94.176/api/';
-export const VERSION = 'v1/';
-export const WORDS_ENDPOINT = API_ROOT+VERSION+'word/';
+const API_ROOT = 'http://34.67.94.176/api/';
+const VERSION = 'v1/';
+const WORDS_ENDPOINT = API_ROOT+VERSION+'word/';
+
+export {WORDS_ENDPOINT}
