02cff8a9dac1273ff7c4e61a2aac9e3ff5054e78
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-06-19 22:00:55 -0500

    auth requests to api

---
+++ app/api/authFetch.js
@@ -0,0 +1,12 @@
+import { getToken } from '../utils/getToken';
+
+const authFetch = async (url, options = {}) => {
+    const token = getToken();
+    if (!options.headers) {
+        options.headers = new Headers();
+    }
+    options.headers.set('authorization', token);
+    return await fetch(url, options);
+};
+
+export default authFetch;
