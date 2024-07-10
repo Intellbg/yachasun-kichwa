d11a7930e899e32801a89d6c741d087d81a0dc2f
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-07-10 18:15:28 -0500

    add registration page

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
-const API_ROOT = 'http://34.67.94.176/api/';
+const API_ROOT = 'http://localhost:3000/api/';
 const VERSION = 'v1/';
 const WORDS_ENDPOINT = API_ROOT+VERSION+'word/';
+const AUTH_ENDPOINT = API_ROOT+VERSION+'auth/';
 
-export {WORDS_ENDPOINT}
+export {WORDS_ENDPOINT,AUTH_ENDPOINT}
