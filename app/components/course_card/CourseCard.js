b33866844f45b4a12adc6682534615a77334cea2
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-09-12 11:29:09 -0500

    fix conditionals warning

--- app/components/course_card/CourseCard.js
+++ app/components/course_card/CourseCard.js
@@ -4,7 +4,7 @@ export default function CourseCard({ data, currentScore }) {
     const unlocked = currentScore < data.score_required
     console.log(unlocked)
     return (
-        <a href={!unlocked && data.url} className="text-decoration-none">
+        <a href={!unlocked ? data.url : ""} className="text-decoration-none">
             <div className={`card m-3 ${unlocked ? "blocked" : "hover-div"}`}>
                 <div className="d-flex">
                     <img src={data.img} height={"200px"} width={"200px"} alt={data.title + " icono"} />
--- app/components/lecture_card/LectureCard.js
+++ app/components/lecture_card/LectureCard.js
@@ -4,7 +4,7 @@ export default function LectureCard({ data, currentScore }) {
     const unlocked = currentScore < data.score_required
     const current = currentScore == data.score_required
     return (
-        <a href={ !unlocked && ("/lectures/basic/es/" + data.slug)} className="text-decoration-none">
+        <a href={ !unlocked ? ("/lectures/basic/es/" + data.slug) : ""} className="text-decoration-none">
             <div className={`card m-3 py-4 ${unlocked ? "blocked" : current ? "current hover-div" : "passed hover-div"}`}>
                 <div className="mx-a">
                     <h2 className="text-center">{data.name}</h2>
