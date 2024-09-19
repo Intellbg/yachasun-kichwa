4417a6e29350aa67def5a1aa9536449882eb9229
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-09-19 08:12:50 -0500

    remove session stuff

--- app/lib/getSession.js
+++
@@ -1,7 +0,0 @@
-import { cookies } from 'next/headers'
-import {jwtDecode} from 'jwt-decode';
-
-export async function getSessionData() {
-    const encryptedSessionData = cookies().get('session')?.value
-    return encryptedSessionData ? jwtDecode(encryptedSessionData) : null
-}
