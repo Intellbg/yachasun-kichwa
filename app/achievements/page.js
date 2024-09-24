9324222f1c3a1ec0761227486da8c82cd16ccebd
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-09-23 21:08:57 -0500

    add navbar to Achievements

--- app/achievements/page.js
+++ app/achievements/page.js
@@ -1,6 +1,7 @@
-"use client"; // Marca este archivo como un componente del cliente
+"use client"; 
 
 import { useState } from 'react';
+import Navbar from "../components/Navbar"
 
 export default function Achievements() {
     const [currentCard, setCurrentCard] = useState(null);
@@ -23,7 +24,6 @@ export default function Achievements() {
             creditos: "Proyecto 'Artesanías de los pueblos Ancestrales en la Mitad del Mundo: Ecuador'",
             imgSrc: "/img/logro2.png",
         },
-        // Agrega más cartas según sea necesario
     ];
 
     const handleCardClick = (index) => {
@@ -31,48 +31,51 @@ export default function Achievements() {
     };
 
     return (
-        <div className="container-fluid p-0">
-            <main className="container my-5">
-                <section className="text-center mb-5">
-                    <h2 className="text-success">Logros</h2>
-                </section>
-                <section className="text-center mb-5">
-                    <h2 className="text-success">Juegos <span>6/6</span></h2>
-                    <div className="d-flex flex-wrap justify-content-center">
-                        {cards.map((card, index) => (
-                            <div key={index} className="m-2" onClick={() => handleCardClick(index)}>
-                                <img src={card.imgSrc} alt={`Juego ${index + 1}`} />
+        <>
+            <Navbar />
+            <div className="container-fluid p-0">
+                <main className="container my-5">
+                    <section className="text-center mb-5">
+                        <h2 className="text-success">Logros</h2>
+                    </section>
+                    <section className="text-center mb-5">
+                        <h2 className="text-success">Juegos <span>6/6</span></h2>
+                        <div className="d-flex flex-wrap justify-content-center">
+                            {cards.map((card, index) => (
+                                <div key={index} className="m-2" onClick={() => handleCardClick(index)}>
+                                    <img src={card.imgSrc} alt={`Juego ${index + 1}`} />
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                    {currentCard !== null && (
+                        <div className="card-detail">
+                            <h3>Detalles del Logro</h3>
+                            <p><strong>COD:</strong> {cards[currentCard].cod}</p>
+                            <p><strong>Área Cultural:</strong> {cards[currentCard].area}</p>
+                            <p><strong>Filiación:</strong> {cards[currentCard].filiacion}</p>
+                            <p><strong>Datación:</strong> {cards[currentCard].datacion}</p>
+                            <p><strong>Fuente:</strong> {cards[currentCard].fuente}</p>
+                            <p><strong>Créditos:</strong> {cards[currentCard].creditos}</p>
+                        </div>
+                    )}
+                    <section className="text-center">
+                        <h2 className="text-success">Examenes <span>6/6</span></h2>
+                        <div className="d-flex flex-wrap justify-content-center">
+                            <div className="m-2">
+                                <img src="/img/logro3.png" alt="Examen 1" />
                             </div>
-                        ))}
-                    </div>
-                </section>
-                {currentCard !== null && (
-                    <div className="card-detail">
-                        <h3>Detalles del Logro</h3>
-                        <p><strong>COD:</strong> {cards[currentCard].cod}</p>
-                        <p><strong>Área Cultural:</strong> {cards[currentCard].area}</p>
-                        <p><strong>Filiación:</strong> {cards[currentCard].filiacion}</p>
-                        <p><strong>Datación:</strong> {cards[currentCard].datacion}</p>
-                        <p><strong>Fuente:</strong> {cards[currentCard].fuente}</p>
-                        <p><strong>Créditos:</strong> {cards[currentCard].creditos}</p>
-                    </div>
-                )}
-                <section className="text-center">
-                    <h2 className="text-success">Examenes <span>6/6</span></h2>
-                    <div className="d-flex flex-wrap justify-content-center">
-                        <div className="m-2">
-                            <img src="/img/logro3.png" alt="Examen 1" />
+                            <div className="m-2">
+                                <img src="/img/logro4.png" alt="Examen 2" />
+                            </div>
+                            <div className="m-2">
+                                <img src="/img/logro5.png" alt="Examen 2" />
+                            </div>
                         </div>
-                        <div className="m-2">
-                            <img src="/img/logro4.png" alt="Examen 2" />
-                        </div>
-                        <div className="m-2">
-                            <img src="/img/logro5.png" alt="Examen 2" />
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </div>
+                    </section>
+                </main>
+            </div>
+        </>
     );
 }
 
