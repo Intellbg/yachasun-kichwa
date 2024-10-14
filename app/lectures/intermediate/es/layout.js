9ecd10faf8aade18c2662e0e6f9a3564f7e4ce1c
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-10-14 00:29:11 -0500

    fix intermediate layout

--- app/lectures/intermediate/es/layout.js
+++ app/lectures/intermediate/es/layout.js
@@ -1,9 +1,21 @@
+"use client"
+import NavBar from '../../../components/Navbar'
+import BackButton from "@/app/components/BackButton"
+
 export default function MdxLayout({ children }) {
+
     return (
-        <div className='container my-5 py-5 h-100'>
-            <div className='card p-5'>
-                {children}
+        <>
+            <NavBar />
+            <div className='container my-1 py-5 h-100'>
+                <div className='card p-5'>
+                    {children}
+                </div>
+                <div className="d-flex justify-content-center align-items-center">
+                    <BackButton></BackButton>
+                </div>
             </div>
-        </div>
+
+        </>
     )
 }
