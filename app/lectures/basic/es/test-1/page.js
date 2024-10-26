f996715e00f9e210ba4c5c0ddff7e16037007906
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-10-26 08:46:30 -0500

    finish exams

--- app/lectures/basic/es/test-1/page.js
+++ app/lectures/basic/es/test-1/page.js
@@ -5,6 +5,7 @@ import { useAuthStore } from '@/providers/auth-store-provider.js'
 import { USER_ENDPOINT } from "@/constants.js"
 import { getQuestions } from "@/app/lib/getQuestions.js";
 import ComicSpeechBubble from "@/app/components/ComicSpeechBubble/ComicSpeechBubble.js"
+import animation from "@/app/animation.module.css"
 
 export default function Test1() {
   const { level, addLevel, id, key } = useAuthStore(
@@ -57,15 +58,15 @@ export default function Test1() {
       setHumuExpression(`humuSad`);
     }
   };
-  
+
   useEffect(() => {
     setIsCorrect(Array(questions.length).fill(true))
   }, [questions]);
 
   return (
     <div className="container">
-      <h1 className="text-center">Evaluación 1</h1>
       <div className={`${(5 < level) | isAnswerCorrect ? "d-none" : ""}`}>
+        <h1 className="text-center">Evaluación 1</h1>
         {questions.map((questionData, index) => (
           <Question
             key={index}
@@ -79,7 +80,19 @@ export default function Test1() {
         <button className={`btn btn-success ${isAnswerCorrect ? "d-none" : ""}`} onClick={checkAnswers} level={level}>Enviar</button>
         {result && <ComicSpeechBubble text={result} character={humuExpression} />}
       </div>
-      <a href={`/lectures/basic/es/greetings-farewells`} className={`text-center btn btn-success ${(5 < level) | isAnswerCorrect ? "" : "d-none"}`}>Continuar</a>
+      <div className={`${!((5 < level)) ? "d-none" : ""} text-center`}>
+        <h1>Felicitaciones ya ha completado el módulo 1 del nivel básico!</h1>
+        <img
+          src="/img/humu/humu-talking.png"
+          height={300}
+          className={`humu-mascot me-4 ${animation.spinnerImage}`}
+        />
+        <br />
+        <h3 s><a href="/achievements">Revisa tus Logros</a></h3>
+        <div className="m-auto text-center">
+          <a href={`/lectures/basic/es/greetings-farewells`} className={`text-center btn btn-success ${(5 < level) | isAnswerCorrect ? "" : "d-none m-auto"}`}>Continuar</a>
+        </div>
+      </div>
     </div>
   );
 };
--- app/lectures/basic/es/test-2/page.js
+++ app/lectures/basic/es/test-2/page.js
@@ -1,10 +1,11 @@
 "use client"
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Question from "@/app/components/Question";
 import { useAuthStore } from '@/providers/auth-store-provider.js'
 import { USER_ENDPOINT } from "@/constants.js"
 import { getQuestions } from "@/app/lib/getQuestions.js";
 import ComicSpeechBubble from "@/app/components/ComicSpeechBubble/ComicSpeechBubble.js"
+import animation from "@/app/animation.module.css"
 
 export default function Test1() {
   const { level, addLevel, id, key } = useAuthStore(
@@ -64,8 +65,8 @@ export default function Test1() {
 
   return (
     <div className="container">
-      <h1 className="text-center">Evaluación 2</h1>
-      <div className={`${(17 < level) | isAnswerCorrect ? "d-none" : ""}`}>
+      <div className={`${(12 < level) | isAnswerCorrect ? "d-none" : ""}`}>
+        <h1 className="text-center">Evaluación 2</h1>
         {questions.map((questionData, index) => (
           <Question
             key={index}
@@ -78,9 +79,20 @@ export default function Test1() {
         ))}
         <button className={`btn btn-success ${isAnswerCorrect ? "d-none" : ""}`} onClick={checkAnswers} level={level}>Enviar</button>
         {result && <ComicSpeechBubble text={result} character={humuExpression} />}
-
       </div>
-      <a href={`/lectures/basic/es/house`} className={`text-center btn btn-success ${(17 < level) | isAnswerCorrect ? "" : "d-none"}`}>Continuar</a>
+      <div className={`${(12 >= level) ? "d-none" : ""} text-center`}>
+        <h1>Felicitaciones ya ha completado el módulo 2 del nivel básico!</h1>
+        <img
+          src="/img/humu/humu-talking.png"
+          height={300}
+          className={`humu-mascot me-4 ${animation.spinnerImage}`}
+        />
+        <br />
+        <h3 s><a href="/achievements">Revisa tus Logros</a></h3>
+        <div className="m-auto text-center">
+          <a href={`/lectures/basic/es/greetings-farewells`} className={`text-center btn btn-success ${(12 < level) | isAnswerCorrect ? "" : "d-none m-auto"}`}>Continuar</a>
+        </div>
+      </div>
     </div>
   );
 };
