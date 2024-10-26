413938b5e2be72620d465c817d2f07bcc878a91f
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-10-26 03:13:53 -0500

    parametrize helper

--- app/components/helper/Helper.js
+++ app/components/helper/Helper.js
@@ -2,32 +2,36 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const Helper = ({ imageSrc, children }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const Helper = ({ imageSrc, children, style, className, h=150, isOpen=false }) => {
+  const [isModalOpen, setIsModalOpen] = useState(isOpen);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+  if (!style){
+    style = {
+      position: 'absolute',
+      bottom: '20px',
+      right: '20px',
+      cursor: 'pointer',
+      zIndex: 1000,
+    }
+  }
 
   return (
     <>
       <img
         src={imageSrc}
         alt="Open Modal"
-        style={{
-          position: 'absolute',
-          bottom: '20px',
-          right: '20px',
-          cursor: 'pointer',
-          zIndex: 1000,
-        }}
-        height={150}
+        style={style}
+        className={`${className}`}
+        height={h}
         onClick={toggleModal}
       />
 
       {isModalOpen && (
         <div className="modal-overlay" onClick={toggleModal}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()} style={{maxHeight:"80%", minHeight:"40%"}}>
             <button className="close-button" onClick={toggleModal}>
               X
             </button>
