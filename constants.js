5d9ae3cf5cc5f22d48265ac533bef1a36c6bc0f3
Nicolas Burbano <nicolas.rnbp@gmail.com>
2025-01-10 20:08:35 -0500

    Update api

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const API_ROOT = 'http://localhost:3000/api/';
+const API_ROOT = 'https://api.yachasunkichwa.usagicli.com/api/';
 const VERSION = 'v1/';
 const WORDS_ENDPOINT = API_ROOT + VERSION + 'word/';
 const AUTH_ENDPOINT = API_ROOT + VERSION + 'auth/';
