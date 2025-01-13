451d5729064143fd9894123ab088f6ea2bb8728b
Fausto Universidad Roman <fausto.roman@epn.edu.ec>
2025-01-13 16:52:48 -0500

    Se modifico las instrucciones

--- app/components/instructions/completesentence/CompleteSentenceInstructions.js
+++ app/components/instructions/completesentence/CompleteSentenceInstructions.js
@@ -11,13 +11,12 @@ const CompleteSentenceInstructions = () => {
             </p>         
             <h2 className={styles.header}>Instrucciones</h2>
             <p className={styles.paragraph}>
-                <strong>1. Lee atentamente la oración:</strong> Observa el espacio vacío marcado con una línea (&quot;____&quot;) en la oración.<br />
-                <strong>2. Revisa las opciones de palabras:</strong> En la parte inferior de la pantalla encontrarás varias palabras como posibles respuestas.<br />
-                <strong>3. Arrastra y coloca tu respuesta:</strong> Selecciona la palabra que creas correcta, haz clic y arrástrala hasta el espacio vacío en la oración, luego suéltala en su lugar.<br />
-                <strong>4. Verifica y ajusta si es necesario:</strong> Si has elegido correctamente, recibirás un mensaje de felicitación. Si no, intenta con otra palabra.
+                1. Observa el espacio vacío marcado con una línea (&quot;____&quot;) en la oración.<br />
+                2. En la parte inferior de la pantalla encontrarás varias palabras como posibles respuestas.<br />
+                3. Selecciona la palabra que creas correcta, haz clic y arrástrala hasta el espacio vacío en la oración, luego suéltala en su lugar.<br />
+                4. Si has elegido correctamente, recibirás un mensaje de felicitación. Si no, intenta con otra palabra.
             </p>
-        </div>  
-        
+        </div>          
     );
 };
 
--- app/components/instructions/soupletter/SoupLetterInstruction.js
+++ app/components/instructions/soupletter/SoupLetterInstruction.js
@@ -1,28 +1,22 @@
 import React from 'react';
+import styles from './SoupLetterInstructions.module.css';
 
 const SoupLetterInstructions = () => {
     return (
-        <div className="container mt-5  justify-content-center">
-            <div className="text-center">
-                <h2>¿Cómo Jugar &quot;Sopa de letras&quot;?</h2>
-            </div>
-            <ol className="list-group list-group-numbered">
-                <li className="list-group-item">
-                    <strong>Busca las palabras en la lista:</strong> Observa la lista de palabras que necesitas encontrar en la sopa de letras. Están ubicadas en diferentes direcciones dentro del tablero.
-                </li>
-                <li className="list-group-item">
-                    <strong>Explora el tablero:</strong> Examina cuidadosamente cada fila, columna y diagonal en busca de las palabras de la lista.
-                </li>
-                <li className="list-group-item">
-                    <strong>Selecciona las palabras encontradas:</strong> Una vez que encuentres una palabra, utiliza el mouse para seleccionarla, marcándola desde la primera hasta la última letra.
-                </li>
-                <li className="list-group-item">
-                    <strong>Confirma tus hallazgos:</strong> Cuando selecciones una palabra correctamente, se marcará automáticamente en la lista y en el tablero. Continúa hasta completar todas las palabras.
-                </li>
-                <p>¡Diviértete buscando y encuentra todas las palabras para completar el desafío!</p>
-            </ol>
+        <div className="container text-left">
+            <h1 className={styles.header}>¿Cómo Jugar Sopa de letras?</h1>
+            <p className={styles.paragraph}>
+                Sopaletras es un juego donde debes encontrar todas las palabras ocultas en un tablero lleno de letras. 
+                Pon a prueba tu agudeza visual y resuelve el desafío.
+            </p>
+            <h2 className={styles.header}>Instrucciones</h2>
+            <p className={styles.paragraph}>
+                1. Observa la lista de palabras que necesitas encontrar en la sopa de letras. Están ubicadas en diferentes direcciones dentro del tablero.<br />
+                2. Examina cuidadosamente cada fila, columna y diagonal en busca de las palabras de la lista.<br />
+                3. Una vez que encuentres una palabra, utiliza el mouse para seleccionarla, marcándola desde la primera hasta la última letra.<br />
+                4. Cuando selecciones una palabra correctamente, se marcará automáticamente en la lista y en el tablero. Continúa hasta completar todas las palabras.
+            </p>
         </div>
     );
 };
-
 export default SoupLetterInstructions;
